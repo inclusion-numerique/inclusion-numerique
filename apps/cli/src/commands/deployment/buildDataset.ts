ea2d9e08974f6e313924e708cbdc686dbeb5d94b
Hugues Maignol <hugues@kime.tech>
2023-07-25 17:11:43 +0200

    ci: add retry strategy for dataset build post deploy

--- apps/cli/src/commands/deployment/buildDataset.ts
+++ apps/cli/src/commands/deployment/buildDataset.ts
@@ -1,5 +1,7 @@
-import axios from 'axios'
+import * as process from 'node:process'
+import axios, { AxiosError } from 'axios'
 import { Command } from '@commander-js/extra-typings'
+import axiosRetry from 'axios-retry'
 import { output } from '@app/cli/output'
 
 export const buildDataset = new Command()
@@ -10,6 +12,20 @@ export const buildDataset = new Command()
       baseURL: url,
     })
 
-    const result = await client.post<{ status: string }>('/jobs/build-data')
-    output(result.data)
+    axiosRetry(client, {
+      retries: 3,
+      retryDelay: (retryCount) => retryCount * 5000,
+    })
+
+    try {
+      const result = await client.post<{ status: string }>('/jobs/build-data')
+      output(result.data)
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        output(error.response?.data)
+      } else {
+        output(error)
+      }
+      process.exit(1)
+    }
   })
