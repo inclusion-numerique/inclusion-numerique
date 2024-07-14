e2cd18959171819a18aa276532ade8b88cad6c77
Fabien Mercier <fabien.mercier@octo.com>
2024-07-14 15:16:59 +0200

    Ajout de la migration des champs gouvernances.commentaire_suivi et gouvernances.probleme_identifie

---
+++ apps/web/prisma/migrations/20240714131333_ajout_champ_gouvernances_commentaire_suivi/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "gouvernances" ADD COLUMN "probleme_identifie" TEXT NOT NULL DEFAULT '';
+ALTER TABLE "gouvernances" ADD COLUMN "commentaire_suivi" TEXT NOT NULL DEFAULT '';
